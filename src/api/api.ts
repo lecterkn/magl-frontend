6d37837fdc148bf017ae9bfa57754bcb3055a7b0
lecterkn <lecterkn@proton.me>
2025-04-04 18:41:56 +0900

    マイリスト追加をバックエンドと疎通 #4

--- src/api/api.ts
+++ src/api/api.ts
@@ -26,6 +26,25 @@ import { BASE_PATH, COLLECTION_FORMATS, BaseAPI, RequiredError, operationServerM
 /**
  * 
  * @export
+ * @interface RequestMyListAddRequest
+ */
+export interface RequestMyListAddRequest {
+    /**
+     * 
+     * @type {number}
+     * @memberof RequestMyListAddRequest
+     */
+    'score'?: number;
+    /**
+     * 
+     * @type {string}
+     * @memberof RequestMyListAddRequest
+     */
+    'storyId'?: string;
+}
+/**
+ * 
+ * @export
  * @interface RequestUserSigninRequest
  */
 export interface RequestUserSigninRequest {
@@ -735,6 +754,119 @@ export class CategoryApi extends BaseAPI {
      */
     public categoriesPost(name: string, description: string, image?: File, options?: RawAxiosRequestConfig) {
         return CategoryApiFp(this.configuration).categoriesPost(name, description, image, options).then((request) => request(this.axios, this.basePath));
+    }
+}
+
+
+
+/**
+ * MylistApi - axios parameter creator
+ * @export
+ */
+export const MylistApiAxiosParamCreator = function (configuration?: Configuration) {
+    return {
+        /**
+         * マイリストにストーリーを追加
+         * @summary AddMyList
+         * @param {RequestMyListAddRequest} request マイリスト追加リクエスト
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        mylistsPost: async (request: RequestMyListAddRequest, options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+            // verify required parameter 'request' is not null or undefined
+            assertParamExists('mylistsPost', 'request', request)
+            const localVarPath = `/mylists`;
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'POST', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+            // authentication BearerAuth required
+            await setApiKeyToObject(localVarHeaderParameter, "Authorization", configuration)
+
+
+    
+            localVarHeaderParameter['Content-Type'] = 'application/json';
+
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+            localVarRequestOptions.data = serializeDataIfNeeded(request, localVarRequestOptions, configuration)
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
+    }
+};
+
+/**
+ * MylistApi - functional programming interface
+ * @export
+ */
+export const MylistApiFp = function(configuration?: Configuration) {
+    const localVarAxiosParamCreator = MylistApiAxiosParamCreator(configuration)
+    return {
+        /**
+         * マイリストにストーリーを追加
+         * @summary AddMyList
+         * @param {RequestMyListAddRequest} request マイリスト追加リクエスト
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async mylistsPost(request: RequestMyListAddRequest, options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<void>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.mylistsPost(request, options);
+            const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
+            const localVarOperationServerBasePath = operationServerMap['MylistApi.mylistsPost']?.[localVarOperationServerIndex]?.url;
+            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
+        },
+    }
+};
+
+/**
+ * MylistApi - factory interface
+ * @export
+ */
+export const MylistApiFactory = function (configuration?: Configuration, basePath?: string, axios?: AxiosInstance) {
+    const localVarFp = MylistApiFp(configuration)
+    return {
+        /**
+         * マイリストにストーリーを追加
+         * @summary AddMyList
+         * @param {RequestMyListAddRequest} request マイリスト追加リクエスト
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        mylistsPost(request: RequestMyListAddRequest, options?: RawAxiosRequestConfig): AxiosPromise<void> {
+            return localVarFp.mylistsPost(request, options).then((request) => request(axios, basePath));
+        },
+    };
+};
+
+/**
+ * MylistApi - object-oriented interface
+ * @export
+ * @class MylistApi
+ * @extends {BaseAPI}
+ */
+export class MylistApi extends BaseAPI {
+    /**
+     * マイリストにストーリーを追加
+     * @summary AddMyList
+     * @param {RequestMyListAddRequest} request マイリスト追加リクエスト
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof MylistApi
+     */
+    public mylistsPost(request: RequestMyListAddRequest, options?: RawAxiosRequestConfig) {
+        return MylistApiFp(this.configuration).mylistsPost(request, options).then((request) => request(this.axios, this.basePath));
     }
 }
 
--- src/components/home/list/dialog/story_add_dialog.tsx
+++ src/components/home/list/dialog/story_add_dialog.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Dialog,
   DialogContent,
@@ -10,6 +12,9 @@ import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
 import { useState } from "react";
 import StarSelector from "./story_add_dialog_score_selector";
+import { Configuration, MylistApiFactory } from "@/api";
+import { API_HOST_BASEPATH } from "@/api/global";
+import { useAuthStore } from "@/store/user";
 
 interface Props {
   story: StoryModel;
@@ -24,7 +29,35 @@ const StoryAddDialog: React.FC<Props> = ({
   setOpen,
   addList,
 }) => {
-  const [value, setValue] = useState<string | null>(null);
+  const [value, setValue] = useState<number | null>(null);
+  const { auth } = useAuthStore();
+  const onSubmit = () => {
+    if (!value) {
+      toast("score is not set!");
+      return;
+    }
+    const config = new Configuration({
+      basePath: API_HOST_BASEPATH,
+      apiKey: "Bearer " + auth?.accessToken,
+    });
+    MylistApiFactory(config)
+      .mylistsPost({
+        storyId: story.id,
+        score: value,
+      })
+      .then((response) => {
+        if (response.status != 204) {
+          toast("failed to add to MyList");
+          addList(story.id);
+          setOpen(false);
+          return;
+        }
+        toast("added " + story.title + "(" + value + ")");
+      })
+      .catch(() => {
+        toast("failed to add to MyList");
+      });
+  };
   return (
     <Dialog open={isOpen} onOpenChange={setOpen}>
       <DialogContent className="bg-gray-100 sm:max-w-[425px]">
@@ -40,14 +73,7 @@ const StoryAddDialog: React.FC<Props> = ({
           >
             Close
           </Button>
-          <Button
-            className="bg-blue-600 hover:bg-blue-700"
-            onClick={() => {
-              addList(story.id);
-              toast('add "' + story.title + '" to list (' + value + ")");
-              setOpen(false);
-            }}
-          >
+          <Button className="bg-blue-600 hover:bg-blue-700" onClick={onSubmit}>
             Save
           </Button>
         </DialogFooter>
